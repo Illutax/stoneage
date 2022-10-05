a0d4f7c0a3839645688dba595c08e1692269dfbf
Valerij Dobler <vd@wps.de>
2022-10-05 22:48:38 +0200

    Init

---
+++ src/test/kotlin/tech/dobler/stoneage/StoneageApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.dobler.stoneage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoneageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
